f5301093c6e3d2089d144698d9bf742f7890aa72
Nicholas Zafiriou <nicholaszafiriou@gmail.com>
2020-02-13 18:41:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # My Code here....
+def map_to_negativize(src)
+  i = 0 
+  arr = []
+  while i < src.size do
+    arr[i] = src[i] * -1
+  end
+  arr
+end
+
+def map_to_no_change(src)
+  src
+end
+
+def map_to_double(src)
+  i = 0 
+  arr = []
+  while i < src.size do
+    arr[i] = src[i] * 2 
+  end
+  arr
+end
+
+def map_to_square(src)
+  i = 0 
+  arr = []
+  while i < src.size do
+    arr[i] = src[i]**2 
+  end
+end
+  
